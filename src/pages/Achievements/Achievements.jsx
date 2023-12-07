71cd60b33b728d31bff66b620ad49d80f233e74e
Albert Tazetdinov <zet565758@gmail.com>
2023-12-07 11:30:02 +0300

    добавил комментарий

--- src/pages/Achievements/Achievements.jsx
+++ src/pages/Achievements/Achievements.jsx
@@ -9,6 +9,8 @@ const listAchive = [
     {nameLevel: 'Знаток', listAchive: [{numberParty: 1, unlock: true}, {numberParty: 2, unlock: true}, {numberParty: 3, unlock: false}]},
 ]
 
+//unlock добавил, чтобы проверить на заблокированность ачивки
+
 export const Achievements = () => {
     return (
         <div className={cl.achievements}>
