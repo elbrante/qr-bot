5b851c0aacf9858bfdeb756ce8339f594ab10f14
Albert Tazetdinov <zet565758@gmail.com>
2023-12-09 14:48:56 +0300

    добавил запрос на получение ачивок

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,8 @@
 import axios, * as others from 'axios';
 
-axios.defaults.baseURL = 'https://thx-api-server.ru/prosugar'
+axios.defaults.baseURL = 'https://thx-api-server.ru/qrbot'
 
 
+export const getListAchive = () => {
+    return axios.get('/getListAchive/')
+}
--- src/pages/Achievements/Achievements.jsx
+++ src/pages/Achievements/Achievements.jsx
@@ -1,6 +1,8 @@
 import cl from './Achievements.module.css'
 import level1 from '../../assets/unlock/UnLockLevel1.png'
 import {BlockAchive} from "./BlockAchive/BlockAchive";
+import {useEffect, useState} from "react";
+import {getListAchive} from "../../api/api";
 
 
 const listAchive = [
@@ -12,6 +14,16 @@ const listAchive = [
 //unlock добавил, чтобы проверить на заблокированность ачивки
 
 export const Achievements = () => {
+    const [listAchives, setListAchive] = useState([])
+
+    useEffect(() => {
+        getListAchive().then(res => {
+            setListAchive(res.data)
+            console.log(res.data)
+        })
+    }, []);
+
+
     return (
         <div className={cl.achievements}>
             <header className={cl.header}>
